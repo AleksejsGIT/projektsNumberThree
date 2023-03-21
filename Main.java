34aefaf11a1c02dbbf3795e8a27b941628064be7
Alex 221RDB494 <>
2023-03-21 20:52:15 +0000

    readme pievienots

---
+++ Main.java
@@ -0,0 +1,14 @@
+/*
+3.grupa
+Aleksejs Šepeļevs, 221RDB494
+Anna Kate Zbitkovska, 221RDB208
+Anna Lipenberga, 221RDB237
+*/
+
+
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
